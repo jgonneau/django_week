88872de08fdd501c86c567e6db44b55e2ace2fec
Jean Paul Gonneau <jgonneau@j-p.work>
2019-09-10 16:11:52 +0200

    clean code

---
+++ back/static/chatapp.js
@@ -0,0 +1,11 @@
+$("document").ready(function() {
+  // Initialisation wow
+  new WOW().init();
+  // Prochaine implementation de socket.io
+  // pour communiquer avec le Back ..
+  /*
+    var io = require('socket.io')(80);
+  
+   .. io.emit('tweet', tweet);
+  */
+});
